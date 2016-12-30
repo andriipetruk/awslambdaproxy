d396ec737e657932adfd27256f9919e8cee4eb3c
Dan Vittegleo <dan.vittegleo@hds.com>
2016-12-30 01:06:10 -0800

    playing around with connection management

--- tunnelconnection.go
+++ tunnelconnection.go
@@ -13,12 +13,15 @@ import (
 type TunnelConnection struct {
 	conn net.Conn
 	sess *yamux.Session
+	time time.Time
 }
 
 type TunnelConnectionManager struct {
-	listener net.Listener
-	lastTunnel TunnelConnection
-	mutex sync.RWMutex
+	listener      net.Listener
+	currentTunnel string
+	tunnels       map[string]TunnelConnection
+	mutex         sync.RWMutex
+	emergencyTunnel chan bool
 }
 
 func (t *TunnelConnectionManager) run() {
@@ -38,8 +41,44 @@ func (t *TunnelConnectionManager) run() {
 		log.Println("Established session to", tunnelSession.RemoteAddr())
 
 		t.mutex.Lock()
-		t.lastTunnel = TunnelConnection{c, tunnelSession}
+		t.currentTunnel = c.RemoteAddr().String()
+		t.tunnels[t.currentTunnel] = TunnelConnection{c, tunnelSession, time.Now()}
 		t.mutex.Unlock()
+		go t.monitorConnectionHealth(c.RemoteAddr().String())
+		log.Println("Active tunnel count: ", len(t.tunnels))
+	}
+}
+
+func (t *TunnelConnectionManager) monitorConnectionHealth(connectionId string) {
+	for {
+		_, err := t.tunnels[connectionId].sess.Ping()
+		if err != nil {
+			if time.Since(t.tunnels[connectionId].time).Seconds() < 10 {
+				log.Println("Starting emergency tunnel due to seconds: ", time.Since(t.tunnels[connectionId].time).Seconds())
+				t.emergencyTunnel <- true
+			}
+			t.tunnels[connectionId].sess.Close()
+			t.tunnels[connectionId].conn.Close()
+			t.mutex.Lock()
+			delete(t.tunnels, connectionId)
+			t.mutex.Unlock()
+			break
+		}
+		time.Sleep(time.Millisecond * 250)
+	}
+}
+
+func (t *TunnelConnectionManager) getActiveSession() (net.Conn, error) {
+	for {
+		t.mutex.RLock()
+		tunnel, ok := t.tunnels[t.currentTunnel]
+		t.mutex.RUnlock()
+		if ok {
+			sess, err := tunnel.sess.Open()
+			return sess, err
+		}
+		log.Println("getActiveSession failed..")
+		time.Sleep(time.Second * 1)
 	}
 }
 
@@ -55,7 +94,7 @@ func (t *TunnelConnectionManager) waitUntilReady() {
 }
 
 func (t *TunnelConnectionManager) isReady() bool {
-	if t.lastTunnel == (TunnelConnection{}) {
+	if t.currentTunnel == "" {
 		return false
 	} else {
 		return true
@@ -67,7 +106,13 @@ func newTunnelConnectionManager(port string) (*TunnelConnectionManager, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to start TunnelConnectionManager")
 	}
-	return &TunnelConnectionManager{listener: listener}, nil
+	connectionManager := &TunnelConnectionManager{
+		listener: listener,
+		tunnels: make(map[string]TunnelConnection),
+		emergencyTunnel: make(chan bool),
+	}
+	go connectionManager.run()
+	return connectionManager, nil
 }
 
 func startTunnelListener(tunnelPort string) (net.Listener, error) {
